37f3a28f7a35e2dfaa961704ae248e4a094d897a
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-04-03 16:36:23 +0100

    refactor: consume graph-browser via nix flake input
    nix build produces the complete site:
    - index.html + index.js from graph-browser package
    - data/ from this repo (config, graph, tutorials)
    
    No curl, no CI-time fetching. Pure Nix derivation.
    Consumer just needs: graph-browser flake input + data/.

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    graph-browser.url = "github:lambdasistemi/graph-browser";
+  };
+
+  outputs =
+    { nixpkgs, graph-browser, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          browser = graph-browser.packages.${system}.default;
+        in
+        {
+          default = pkgs.runCommand "cardano-governance-graph" { } ''
+            mkdir -p $out
+            cp ${browser}/index.html $out/
+            cp ${browser}/index.js $out/
+            cp -r ${./data} $out/data
+          '';
+        }
+      );
+    };
+}

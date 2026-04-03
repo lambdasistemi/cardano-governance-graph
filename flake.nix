7ec425e71984c341a7d8ebc79bf205a758ad753d
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-04-03 18:48:46 +0100

    test: pin graph-browser with persistence fix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     # Pin to fix/nix-build-deps until merged
-    graph-browser.url = "github:lambdasistemi/graph-browser/000e304";
+    graph-browser.url = "github:lambdasistemi/graph-browser/c97a034";
   };
 
   outputs =
